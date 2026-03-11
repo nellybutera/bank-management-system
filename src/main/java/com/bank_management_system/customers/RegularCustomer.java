fdeb666b11c577e6995d61a2c31e6395c6793dce
nellybutera <buteranelly250@gmail.com>
2026-03-11 13:15:31 +0200

    added customer classes

---
+++ src/main/java/com/bank_management_system/customers/RegularCustomer.java
@@ -0,0 +1,24 @@
+package com.bank_management_system.customers;
+
+public class RegularCustomer extends Customer {
+
+    public RegularCustomer(String name, int age, String contact, String address){
+        super(name, age, contact, address);
+    }
+
+    @Override
+    public void displayCustomerDetails(){
+        System.out.println(" Customer ID : " + getCustomerId());
+        System.out.println(" Name        : " + getName());
+        System.out.println(" Type        : " + getCustomerType());
+        System.out.println(" Age         : " + getAge());
+        System.out.println(" Contact     : " + getContact());
+        System.out.println(" Address     : " + getAddress());
+    }
+
+    @Override
+    public String getCustomerType(){
+        return "Regular";
+    }
+
+}
