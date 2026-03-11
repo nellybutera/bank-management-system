2a8baa989ab7970c4cc5121a653220562097c9c9
nellybutera <buteranelly250@gmail.com>
2026-03-11 13:15:49 +0200

    added transaction classes

---
+++ src/main/java/com/bank_management_system/Main.java
@@ -0,0 +1,7 @@
+package com.bank_management_system;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/bank_management_system/shared/Transactable.java
@@ -0,0 +1,6 @@
+package com.bank_management_system.shared;
+
+public interface Transactable{
+
+    boolean processTransaction(double amount, String type);
+}
