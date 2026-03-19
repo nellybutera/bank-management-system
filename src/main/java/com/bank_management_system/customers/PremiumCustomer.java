a28b54b0884c7d7b0f9fcc27687eb6c2ccaa8a38
nellybutera <buteranelly250@gmail.com>
2026-03-19 10:51:27 +0200

    added javadoc comments

--- src/main/java/com/bank_management_system/customers/PremiumCustomer.java
+++ src/main/java/com/bank_management_system/customers/PremiumCustomer.java
@@ -1,23 +1,33 @@
 package com.bank_management_system.customers;
 
 public class PremiumCustomer extends Customer {
+
     private static final double minimumBalance = 10000;
 
-    public PremiumCustomer(String name, int age, String contact, String address){
+    public PremiumCustomer(String name, int age, String contact, String address) {
         super(name, age, contact, address);
     }
 
-    public double getMinimumBalance(){
-        return minimumBalance;
-    }
+    /**
+     * Returns the minimum balance required for a Premium account.
+     *
+     * @return the minimum balance amount
+     */
+    public double getMinimumBalance() { return minimumBalance; }
 
+    /**
+     * Premium customers are always eligible for a monthly fee waiver.
+     *
+     * @return true always
+     */
     @Override
-    public boolean isEligibleForFeeWaiver(){
-        return true;
-    }
+    public boolean isEligibleForFeeWaiver() { return true; }
 
+    /**
+     * Prints a formatted summary of this customer's profile details.
+     */
     @Override
-    public void displayCustomerDetails(){
+    public void displayCustomerDetails() {
         System.out.println(" Customer ID : " + getCustomerId());
         System.out.println(" Name        : " + getName());
         System.out.println(" Type        : " + getCustomerType());
@@ -26,9 +36,11 @@ public class PremiumCustomer extends Customer {
         System.out.println(" Address     : " + getAddress());
     }
 
+    /**
+     * Returns the customer tier label.
+     *
+     * @return "Premium"
+     */
     @Override
-    public String getCustomerType(){
-        return "Premium";
-    }
-    
+    public String getCustomerType() { return "Premium"; }
 }
